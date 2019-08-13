9f9cadd2a350ad2b7629ea28bbbaa09c7cbae8f1
dwfig <daniel.w.fries@gmail.com>
2019-08-13 13:10:18 -0400

    added review code

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,76 @@
 class Instructor
 
+  @@all = []
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def boating_tests
+    BoatingTest.all.select do |test|
+      test.instructor == self
+    end
+  end
+
+  def students
+    self.boating_tests.map do |test|
+      test.student
+    end.uniq
+  end
+
+  # def pass_student(student, test_name)
+  #   passed_test = BoatingTest.all.find do |test|
+  #     test.student == student && test.test_name == test_name
+  #   end
+  #   # passed_test.status = "passed"
+  #   if passed_test == nil
+  #     passed_test = BoatingTest.new(student,test_name, "passed", self)
+  #   else
+  #     passed_test.test_status = "passed"
+  #   end
+  #   passed_test
+  #   # If there is a `BoatingTest` whose name and student match the values passed in, this method should update the status of that BoatingTest to 'passed'. If there is no matching test, this method should create a test with the student, that boat test name, and the status 'passed'. Either way, it should return the `BoatingTest` instance.
+  # end
+  #
+  # def fail_student(student, test_name)
+  #   failed_test = BoatingTest.all.find do |test|
+  #     test.student == student && test.test_name == test_name
+  #   end
+  #   # failed_test.status = "passed"
+  #   if failed_test == nil
+  #     failed_test = BoatingTest.new(student,test_name, "failed", self)
+  #   else
+  #     failed_test.test_status = "failed"
+  #   end
+  #   failed_test
+  #   # ` should take in a student instance and test name. Like `#pass_student`, it should try to find a matching `BoatingTest` and update its status to 'failed'. If it cannot find an existing `BoatingTest`, it should create one with the name, the matching student, and the status 'failed'.
+  # end
+
+  def status_change(new_status, student, test_name)
+    test_to_change = self.boating_tests.find do |test|
+      test.student == student && test.test_name == test_name
+    end
+    if test_to_change == nil
+      test_to_change = BoatingTest.new(student, test_name, new_status, self)
+    else
+      test_to_change.test_status = new_status
+    end
+    test_to_change
+  end
+
+  def pass_student(student, test_name)
+    status_change("passed", student, test_name)
+  end
+
+  def fail_student(student, test_name)
+    status_change("failed", student, test_name)
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,67 @@
 class Student
+  # *  initialize with `first_name`
+  attr_reader :first_name
+
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def boating_tests
+    BoatingTest.all.select do |test|
+      test.student == self
+    end
+  end
+
+  def instructors
+    self.boating_tests.map do |test|
+      test.instructor
+    end.uniq
+  end
+
+  def self.find_student(first_name)
+    # output the student (Object) with that name
+    self.all.find do |student|
+        student.first_name == first_name
+    end
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    # initialize new boating test
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def grade_percentage
+    # return the percentage of tests that the student has passed as a Float
+    tests = self.boating_tests
+    passed_tests = self.boating_tests.select do |test|
+      test.test_status == "passed"
+    end
+    num_tests = tests.count.to_f
+    num_passed_tests = passed_tests.count.to_f
+    grade_decimal = num_passed_tests / num_tests
+    grade_percentage = grade_decimal * 100
+  end
+
+  def reduce_gp
+    passed_tests = self.boating_tests.reduce(0) do |accumulator, test|
+      test.test_status == "passed" ? accumulator += 1 : accumulator
+      # if test.test_status == "passed"
+      #   accumulator += 1
+      # end
+      # accumulator
+    end
+  end
+
+  def ternary_version
+    numtests = self.boating_tests.count
+    numtests{|test| test.test_status =="passed"}.to_f/numtests*100
+  end
 
 end
